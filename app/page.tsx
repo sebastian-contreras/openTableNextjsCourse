dc244cbc51ace1a9b6f50b3d0ac1b93e977124cf
Sebastian Contreras <sebasfac2017@gmail.com>
2023-06-05 11:51:08 -0300

    Navbar y header diseno

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className='text-blue-400 text-2xsm'>hola</h1>
-
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="#" className="font-bold text-gray-700 text-2xl">
+            OpenTable
+          </a>
+          <div>
+            <div className="flex">
+              <button className="bg-blue-400 text-white border p-1 px-4 mr-2 rounded">
+                Sign in
+              </button>
+              <button className="border p-1 px-4 rounded">Sign up</button>
+            </div>
+          </div>
+        </nav>
+        {/* FINAL NAVBAR */}
+        <main>
+          {/* HEADER */}
+          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+            <div className="text-center mt-10">
+              <h1 className="text-white text-5xl font-bold mb-2">
+                Find your table for any ocassion
+              </h1>
+          {/* Search Bar */}
+              <div className="text-left text-lg py-3 m-auto flex justify-center">
+                <input type="text" placeholder="State, city or town" className="rounded mr-3 p-2 w-[450px]"/>
+                <button className="rounded font-semibold bg-red-600 px-9 py-2 text-white">Let's Go</button>
+              </div>
+          {/* End Search Bar */}
+            </div>
+          </div>
+          {/* END HEADER */}
+        </main>
+      </main>
     </main>
-  )
+  );
 }
